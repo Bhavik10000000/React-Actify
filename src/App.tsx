d0058ed9c79142e2c25e5558a91dd01f04849856
Bhavik <sapatbhavik101@gmail.com>
2026-06-20 11:16:34 +0530

    Navbar updated

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,18 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="w-full min-h-screen bg-slate-50 flex flex-col">
-        <Tabs defaultValue="task3" className="w-full flex flex-col flex-1">
+        <Tabs defaultValue="task4" className="w-full flex flex-col flex-1">
           <div className="sticky top-0 z-50 w-full h-auto bg-white p-3 shadow-sm flex justify-center border-b">
-            <TabsList className="w-[400px]  grid grid-cols-4">
+            <TabsList className="w-full max-w-3xl h-auto flex flex-wrap justify-center gap-1 p-1 bg-slate-100 rounded-x">
               <TabsTrigger value="task1">Task 1</TabsTrigger>
               <TabsTrigger value="task2">Task 2</TabsTrigger>
               <TabsTrigger value="task3">Task 3</TabsTrigger>
-              <TabsTrigger value="task4">Task 4</TabsTrigger>
+              <TabsTrigger value="task4-5">Task 4-5</TabsTrigger>
+              <TabsTrigger value="task6">Task 6</TabsTrigger>
+              <TabsTrigger value="task7">Task 7</TabsTrigger>
+              <TabsTrigger value="task8">Task 8</TabsTrigger>
+              <TabsTrigger value="task9">Task 9</TabsTrigger>
+              <TabsTrigger value="task10">Task 10</TabsTrigger>
             </TabsList>
           </div>
 
@@ -43,7 +48,37 @@ const App = () => {
               <Task3 />
             </TabsContent>
             <TabsContent
-              value="task4"
+              value="task4-5"
+              className="w-full flex justify-center m-0 outline-none"
+            >
+              <EmptyCom />
+            </TabsContent>
+            <TabsContent
+              value="task6"
+              className="w-full flex justify-center m-0 outline-none"
+            >
+              <EmptyCom />
+            </TabsContent>
+            <TabsContent
+              value="task7"
+              className="w-full flex justify-center m-0 outline-none"
+            >
+              <EmptyCom />
+            </TabsContent>
+            <TabsContent
+              value="task8"
+              className="w-full flex justify-center m-0 outline-none"
+            >
+              <EmptyCom />
+            </TabsContent>
+            <TabsContent
+              value="task9"
+              className="w-full flex justify-center m-0 outline-none"
+            >
+              <EmptyCom />
+            </TabsContent>
+            <TabsContent
+              value="task10"
               className="w-full flex justify-center m-0 outline-none"
             >
               <EmptyCom />
